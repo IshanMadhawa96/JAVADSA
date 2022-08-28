eab3f1a2e470bc92848d99cfdec5404e46a1ca3b
unknown <ishanmadhawa09@gmail.com>
2022-08-28 22:58:07 +0530

    create array

---
+++ Arrays/DSA/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+
+        //Declare Array and size
+        //int [] numbers = new int[3];
+        // System.out.println(numbers); // out put:-[I@7699a589 (Array Memory Location
+        //System.out.println(Arrays.toString(numbers)); // output [0, 0, 0] (Convert Array to String)
+        //Adding value to Array index
+        //numbers [0] =10;
+        //numbers [1] =20;
+        //numbers [2] =30;
+        // print array with values
+        //System.out.println(Arrays.toString(numbers)); //[10, 20, 30]
+
+        //Declare Array Clean and Short Way
+        int[] numbers = {10,20,30};
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("ArrAY Length "+ numbers.length);
+
+
+
+    }
+}
