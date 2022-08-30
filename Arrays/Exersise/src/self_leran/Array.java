e60163406cc79ed0808f9921d9af4467e554c640
unknown <ishanmadhawa09@gmail.com>
2022-08-31 00:02:15 +0530

    add count varible to array clz to trak array element

--- Arrays/Exersise/src/self_leran/Array.java
+++ Arrays/Exersise/src/self_leran/Array.java
@@ -1,14 +1,19 @@
 package self_leran;
 
 public class Array {
+    // define empty array
     private int[] items;
+    //keep track number of items on the array using this count variable
+    private int count;
+
     // constructor
     public Array(int length) {
+        // assign array length via constructer
         items = new int[length];
     }
     //printing array
     public void print(){
-        for(int i=0;i<items.length;i ++){
+        for(int i=0;i<count;i ++){
             System.out.println(items[i]);
         }
     }
