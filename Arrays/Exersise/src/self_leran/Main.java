eaf1f41dcb509895e72b39c8c7d57988dcf8810a
unknown <ishanmadhawa09@gmail.com>
2022-09-03 20:16:56 +0530

    arry list practised

--- Arrays/Exersise/src/self_leran/Main.java
+++ Arrays/Exersise/src/self_leran/Main.java
@@ -1,16 +1,32 @@
 package self_leran;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
-        Array numbers = new Array(3);
-        numbers.insert(10);
-        numbers.insert(20);
-        numbers.insert(30);
-        numbers.insert(40);
-        numbers.insert(50);
-        //numbers.removeAt(2);
-        System.out.println(numbers.indexOf(20));
-        System.out.println(numbers.indexOf(200));
-        //numbers.print();
+//        Array numbers = new Array(3);
+//        numbers.insert(10);
+//        numbers.insert(20);
+//        numbers.insert(30);
+//        numbers.insert(40);
+//        numbers.insert(50);
+//        //numbers.removeAt(2);
+//        System.out.println(numbers.indexOf(20));
+//        System.out.println(numbers.indexOf(200));
+//        //numbers.print();
+        // java has two dynamic arrays
+        //1.Vector-synchronized
+        //2.Array List
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.remove(0);// remove element
+        list.indexOf(20);// find index
+        list.size(); //find array list size
+        System.out.println(list);
+
     }
 }
